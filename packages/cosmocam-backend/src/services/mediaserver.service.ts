b01530f4e737e75c8ff189da3ceedab9b1d0f1f4
alex-clements <ahclemen@ualberta.ca>
2023-08-17 15:17:32 -0700

    Update getMediaServer in cosmocam-backend

--- packages/cosmocam-backend/src/services/mediaserver.service.ts
+++ packages/cosmocam-backend/src/services/mediaserver.service.ts
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const getMediaServer = (data: any) => {
-  return "localhost:3002";
+  let server_list = process.env.MEDIASERVER_IP_LIST?.split(",") || [];
+  return server_list[0];
 };
 
 export const mediaserverService = { getMediaServer };
